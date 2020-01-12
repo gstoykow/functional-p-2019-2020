11c2727c9b5342c178f47f0f111980ad7b8ac6eb
Georgi Stoykow <gstoykow@abv.bg>
2020-01-12 20:03:30 +0200

    hw added

--- 03/src/Trees.hs
+++ 03/src/Trees.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE InstanceSigs #-}
-{-# OPTIONS_GHC -fplugin=HLint #-} -- run hlint on build via the hlint source plugin
+{-# OPTIONS_GHC -fplugin=HLint #-} 
 
 module Trees where
 
@@ -13,45 +13,70 @@ data Tree a
   deriving Show
 
 instance Eq a => Eq (Tree a) where
-  (==) :: Tree a -> Tree a -> Bool
-  (==) = undefined
+
+    (==) :: Eq a => Tree a -> Tree a -> Bool
+    (==) Empty Empty = True
+    (==) _ Empty = False
+    (==) Empty _ = False
+    (==) (Node v1 l1 r1) (Node v2 l2 r2) 
+       | v1 /= v2 = False
+       | otherwise = (==) l1 l2 && (==) r1 r2
 
 insertOrdered :: Ord a => a -> Tree a -> Tree a
-insertOrdered = undefined
+insertOrdered nv Empty = Node nv Empty Empty
+insertOrdered nv (Node v l r) 
+   | v >= nv = Node v (insertOrdered nv l) r
+   | otherwise = Node v l (insertOrdered nv r)
 
 listToBST :: Ord a => [a] -> Tree a
-listToBST = undefined
+listToBST [] = Empty
+listToBST lst = buildTree lst Empty True
+   where buildTree [] t _ = t 
+         buildTree (y:ys) t initFlag
+            | initFlag = buildTree ys (insertOrdered y t) False
+            | otherwise = buildTree ys (insertOrdered y t) False
 
 isBST :: Ord a => Tree a -> Bool
-isBST = undefined
+isBST Empty = True
+isBST (Node _ Empty Empty) = True
+isBST (Node v Empty rn@(Node rv _ _)) 
+   | v < rv = True && isBST rn
+   | otherwise = False
+isBST (Node v ln@(Node lv _ _) Empty) 
+   | v >= lv = True && isBST ln
+   | otherwise = False
+isBST (Node v ln@(Node lv _ _) rn@(Node rv _ _))
+   | v >= lv && v < rv = isBST ln && isBST rn
+   | otherwise = False
 
--- idea for implementing isBST - delete if you don't want it
-data BotTop a = Bot | Val a | Top
-  deriving (Show, Eq, Ord)
-
-between :: Ord a => BotTop a -> BotTop a -> Tree a -> Bool
-between = undefined
 
 findBST :: Ord a => a -> Tree a -> Bool
-findBST = undefined
+findBST _ Empty = False
+findBST sv (Node v l r) 
+   | v == sv = True
+   | v > sv = findBST sv l
+   | otherwise = findBST sv r 
 
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree = undefined
+mapTree _ Empty = Empty
+mapTree f (Node v l r) = Node (f v) (mapTree f l) (mapTree f r)
 
 foldTree :: Monoid a => Tree a -> a
-foldTree = undefined
+foldTree Empty = mempty
+foldTree (Node v l r) = foldTree l <> v <> foldTree r
 
 foldMapTree :: Monoid b => (a -> b) -> Tree a -> b
-foldMapTree = undefined
+foldMapTree _ Empty = mempty
+foldMapTree f (Node v l r) = foldMapTree f l <> f v <> foldMapTree f r
 
 sumTree :: Num a => Tree a -> a
-sumTree = undefined
+sumTree = getSum . foldMapTree Sum
 
 allTree :: (a -> Bool) -> Tree a -> Bool
-allTree = undefined
+allTree p = getAll . foldMapTree (All . p)
 
 treeToList :: Tree a -> [a]
-treeToList = undefined
+treeToList = foldMapTree (:[])
 
 elemTree :: Eq a => a -> Tree a -> Bool
 elemTree = undefined
